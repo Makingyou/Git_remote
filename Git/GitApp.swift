b59725f2d77ab58f2cf76e6e4cbdd48a15af6c53
Makingyou <mansilla.a.alejandro@gmail.com>
2023-04-10 18:08:11 -0300

    Initial Commit

---
+++ Git/GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitApp.swift
+//  Git
+//
+//  Created by Alejandro Mansilla on 10/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
